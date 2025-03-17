97e7a881b636c8e2c5447b117502ce125cefce4e
Hsiaoming Yang <me@lepture.com>
2025-03-17 14:52:52 +0900

    chore: init with models and middleware

---
+++ demo/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path, include
+from django.contrib import admin
+from django.contrib.staticfiles.urls import urlpatterns as static_urlpatterns
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('schema/openapi', SpectacularAPIView.as_view(), name='schema'),
+    path('schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+] + static_urlpatterns
