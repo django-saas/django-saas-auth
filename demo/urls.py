dcc19d2956964c3671bb4980be0677393b01bd4e
Hsiaoming Yang <me@lepture.com>
2025-03-17 15:17:42 +0900

    feat: add sessions api

--- demo/urls.py
+++ demo/urls.py
@@ -9,6 +9,7 @@ from drf_spectacular.views import (
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/sessions/', include('saas_auth.api_urls.session')),
     path('schema/openapi', SpectacularAPIView.as_view(), name='schema'),
     path('schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
