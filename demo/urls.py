9890912acc81a892ef43deee7ebe2960661aef7f
Hsiaoming Yang <me@lepture.com>
2025-03-17 15:41:14 +0900

    feat: add user token and user profile api

--- demo/urls.py
+++ demo/urls.py
@@ -10,6 +10,7 @@ from drf_spectacular.views import (
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/sessions/', include('saas_auth.api_urls.session')),
+    path('api/tokens/', include('saas_auth.api_urls.token')),
     path('schema/openapi', SpectacularAPIView.as_view(), name='schema'),
     path('schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
