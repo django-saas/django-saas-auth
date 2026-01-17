2907cb807a81a122aca0537440a2f597ba800d7b
Hsiaoming Yang <me@lepture.com>
2026-01-18 00:30:34 +0900

    fix!: use BaseSettings

--- src/saas_auth/settings.py
+++ src/saas_auth/settings.py
@@ -1,20 +1,20 @@
 from django.core.signals import setting_changed
-from saas_base.settings import Settings
-
-DEFAULTS = {
-    'ENABLE_GRAVATAR': False,
-    'GRAVATAR_OPTIONS': {'size': 400, 'default': 'identicon'},
-    'LOCATION_RESOLVER': {
-        'backend': 'saas_auth.location.cloudflare.CloudflareBackend',
-    },
-}
+from saas_base.settings import BaseSettings
 
 
-class AuthSettings(Settings):
-    IMPORT_PROVIDERS = [
+class AuthSettings(BaseSettings):
+    SETTINGS_KEY = 'SAAS_AUTH'
+    DEFAULT_SETTINGS = {
+        'ENABLE_GRAVATAR': False,
+        'GRAVATAR_OPTIONS': {'size': 400, 'default': 'identicon'},
+        'LOCATION_RESOLVER': {
+            'backend': 'saas_auth.location.cloudflare.CloudflareBackend',
+        },
+    }
+    IMPORT_SETTINGS = [
         'LOCATION_RESOLVER',
     ]
 
 
-auth_settings = AuthSettings('SAAS_AUTH', defaults=DEFAULTS)
+auth_settings = AuthSettings()
 setting_changed.connect(auth_settings.listen_setting_changed)
