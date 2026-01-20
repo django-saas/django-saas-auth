782404e8e883e59b0592a2c00a8eb34f96bb52a8
Hsiaoming Yang <me@lepture.com>
2026-01-20 21:03:42 +0900

    fix: move mfa to a new package

--- src/saas_auth/models/__init__.py
+++ src/saas_auth/models/__init__.py
@@ -1,18 +1,8 @@
 from .session import Session
 from .token import UserToken
-from .mfa import (
-    MFASettings,
-    MFABackupCode,
-    TOTPDevice,
-    WebAuthnDevice,
-)
 
 
 __all__ = [
     'UserToken',
     'Session',
-    'MFASettings',
-    'MFABackupCode',
-    'TOTPDevice',
-    'WebAuthnDevice',
 ]
--- src/saas_auth/settings.py
+++ src/saas_auth/settings.py
@@ -1,5 +1,4 @@
 from django.core.signals import setting_changed
-from saas_base.settings import saas_settings
 from saas_base.settings import BaseSettings
 
 
@@ -12,23 +11,10 @@ class AuthSettings(BaseSettings):
             'backend': 'saas_auth.location.cloudflare.CloudflareBackend',
         },
         'SESSION_RECORD_INTERVAL': 300,
-        'MFA_STEP_UP_AUTH': 300,  # skip for 5 minutes
-        'TOTP_DIGITS': 6,
-        'TOTP_ALGORITHM': 'SHA1',
-        'TOTP_PERIOD': 30,
-        'TOTP_ISSUER': '',
-        'TOTP_LABEL_FIELD': 'username',
     }
     IMPORT_SETTINGS = [
         'LOCATION_RESOLVER',
     ]
-
-    @property
-    def totp_issuer(self) -> str:
-        if self.TOTP_ISSUER:
-            return self.TOTP_ISSUER
-        site = saas_settings.SITE
-        return site['name']
 
 
 auth_settings = AuthSettings()
